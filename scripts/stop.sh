5d21df44a89c59ada908b51b4fd9a5f2d9bdf3ca
BigFei <364950776@qq.com>
2026-02-06 08:42:52 +0800

    remove volumes when docker compose down

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-docker compose down
+# 停止所有容器并删除相关的卷
+docker compose down -v
+# 强制删除 salt-master 容器（如果存在）
 docker compose rm salt-master -f
+# 强制删除 salt-minion 容器（如果存在）
 docker compose rm salt-minion -f
